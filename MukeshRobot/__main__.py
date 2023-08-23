6ef018ce964162086a17fd659044d8e8471bd5f4
DJ <119585273+Chetanthakur03@users.noreply.github.com>
2023-08-23 10:12:04 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -220,8 +220,11 @@ def start(update: Update, context: CallbackContext):
             x.delete()
             usr = update.effective_user
             lol = update.effective_message.reply_text(
-                PM_START_TEX.format(usr.first_name), parse_mode=ParseMode.MARKDOWN)
-            timeout=60,
+                PM_START_TEXT.format(usr.first_name, BOT_NAME),
+                reply_markup=InlineKeyboardMarkup(buttons),
+                parse_mode=ParseMode.MARKDOWN,
+                timeout=60,
+            )
            
     else:
         update.effective_message.reply_photo(
