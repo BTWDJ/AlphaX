700e632236aadaf58782ae602c21593abf9c02e6
DJ <119585273+Chetanthakur03@users.noreply.github.com>
2023-08-23 09:36:22 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -240,7 +240,7 @@ def error_handler(update, context):
 
     # traceback.format_exception returns the usual python message about an exception, but as a
     # list of strings rather than a single string, so we have to join them together.
- tb_list = traceback.format_exception(
+    tb_list = traceback.format_exception(
         None, context.error, context.error.__traceback__
     )
     tb = "".join(tb_list)
